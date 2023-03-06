3a23b35ec2fcc0709c7410f7a846b5e9b9b6f6af
Henny <85436090+hennypurwadi@users.noreply.github.com>
2023-03-06 21:03:57 +0700

    Add files via upload

--- vid2text.py
+++ vid2text.py
@@ -1,10 +1,10 @@
 
 import streamlit as st
-import io
-import requests
-from pydub import AudioSegment
-import whisper
+import pytube
 import nltk
+from pydub import AudioSegment
+from pydub.utils import which
+import whisper
 
 # Download the NLTK tokenizer models
 nltk.download('punkt')
@@ -12,41 +12,33 @@ nltk.download('punkt')
 # Load the WhisPer speech recognition model
 model = whisper.load_model("base")
 
-# Define a function to transcribe the audio data and return the text
-def transcribe_audio(audio_data):
-    audio_segment = AudioSegment.from_file(audio_data)
-    audio_wav = audio_segment.export(format='wav')
-    text = model.transcribe(audio_wav)
+# Define a function to transcribe the audio file and return the text
+def transcribe_audio(audio_path):
+    text = model.transcribe(audio_path)
     return text['text']
 
 # Define the Streamlit app
 def main():
     st.title("Convert YouTube Audio to Text Transcription")
-
+    
     # Get the YouTube link from the user
     video_url = st.text_input("Enter a YouTube link:")
-
+    
     if video_url:
         try:
             # Download the audio from the YouTube video
-            r = requests.get(video_url)
-            audio_data = io.BytesIO(r.content)
-
-            # Transcribe the audio data and show the text
-            text = transcribe_audio(audio_data)
+            video = pytube.YouTube(video_url)
+            audio_stream = video.streams.filter(only_audio=True).first()
+            audio = AudioSegment.from_file(audio_stream.download(), format="mp4")
+            
+            # Set the path to the ffmpeg and ffprobe executables
+            ffmpeg_path = which("ffmpeg")
+            ffprobe_path = which("ffprobe")
+            
+            # Transcribe the audio file and show the text
+            text = transcribe_audio(audio.set_channels(1).set_frame_rate(16000), ffmpeg=ffmpeg_path, ffprobe=ffprobe_path)
             st.header("Transcription")
             st.write(text)
-
-            # Create a download button for the text file
-            if st.button("Download Text File"):
-                with open("transcription.txt", "w") as f:
-                    f.write(text)
-                st.download_button(
-                    label="Download Transcription",
-                    data=open("transcription.txt", "rb").read(),
-                    file_name="transcription.txt",
-                    mime="text/plain"
-                )
         except Exception as e:
             st.error(str(e))
 
