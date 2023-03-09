b8194e1057c7a03908e877ce72542b7f04898613
Henny <85436090+hennypurwadi@users.noreply.github.com>
2023-03-10 00:07:02 +0700

    Add files via upload

--- vid2text.py
+++ vid2text.py
@@ -1,11 +1,12 @@
 
 import io
-import subprocess
+import wave
 import numpy as np
 import streamlit as st
 import pytube
 import whisper
 import nltk
+from moviepy.video.io.VideoFileClip import VideoFileClip
 
 # Download the NLTK tokenizer models
 nltk.download('punkt')
@@ -15,13 +16,13 @@ model = whisper.load_model("base")
 
 # Define a function to transcribe the audio file and return the text
 def transcribe_audio(audio_data):
-    # Convert the audio data to a WAV file using ffmpeg
-    ffmpeg_command = ['ffmpeg', '-y', '-i', '-', '-f', 'wav', '-']
-    ffmpeg_process = subprocess.Popen(ffmpeg_command, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    wav_data, stderr = ffmpeg_process.communicate(audio_data.getbuffer())
+    # Read the audio data using the wave module
+    with wave.open(audio_data, 'rb') as audio_file:
+        audio_params = audio_file.getparams()
+        audio_frames = audio_file.readframes(audio_params.nframes)
 
-    # Convert the WAV data to a NumPy array
-    audio_array = np.frombuffer(wav_data, dtype=np.int16)
+    # Convert the audio data to a NumPy array
+    audio_array = np.frombuffer(audio_frames, dtype=np.int16)
 
     # Transcribe the audio array and return the text
     txt = model.transcribe(audio_array)
@@ -29,18 +30,24 @@ def transcribe_audio(audio_data):
 
 # Define the Streamlit app
 def main():
-    st.title("Convert YouTube Audio to Text Transcription")
+    st.title("Convert YouTube MP4 Video Audio to Text Transcription")
 
     # Get the YouTube link from the user
     video_url = st.text_input("Enter a YouTube link:")
 
     if video_url:
         try:
-            # Download the audio from the YouTube video to memory
+            # Download the video from the YouTube link
             video = pytube.YouTube(video_url)
-            audio = video.streams.filter(only_audio=True).first()
+            video_data = io.BytesIO()
+            video.stream_to_buffer(video_data)
+            video_data.seek(0)
+
+            # Extract the audio from the video and convert to a NumPy array
+            video_clip = VideoFileClip(video_data)
+            audio_clip = video_clip.audio
             audio_data = io.BytesIO()
-            audio.stream_to_buffer(audio_data)
+            audio_clip.write_audiofile(audio_data, fps=16000, nbytes=2, codec='pcm_s16le')
             audio_data.seek(0)
 
             # Transcribe the audio file and show the text
